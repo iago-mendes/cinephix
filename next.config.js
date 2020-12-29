90dd93ef242a8864287b7ce9c43417b24665926d
Iago <iagobrazmendes@gmail.com>
2020-12-29 17:57:19 -0300

    first commit

---
+++ next.config.js
@@ -0,0 +1,15 @@
+const withImages = require('next-images')
+
+module.exports = withImages({
+	esModule: true,
+	images:
+	{
+		// domains: [process.env.API_HOSTNAME],
+		loader: 'imgix',
+		path: ''
+	},
+	publicRuntimeConfig:
+	{
+		
+	}
+})
