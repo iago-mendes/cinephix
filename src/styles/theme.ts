e5352b97c558888a4fc27c3b17eaa0014ae2bb58
Iago <iagobrazmendes@gmail.com>
2021-01-08 10:31:16 -0300

    simplify theme colors

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,15 +1,12 @@
 const theme =
 {
-  colors:
-  {
-		blue: '#161C50',
-		red: '#420C14',
-		redDark: '#26070B',
-		orange: '#FF8A00',
-		green: '#245655',
-		white: '#D4D4F7',
-		black: '#23232A'
-  }
+	blue: '#161C50',
+	red: '#420C14',
+	redDark: '#26070B',
+	orange: '#FF8A00',
+	green: '#245655',
+	white: '#D4D4F7',
+	black: '#23232A'
 }
 
 export default theme
