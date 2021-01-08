f19bca197f2fe833227b8e58096c7b67ea7ba206
Iago <iagobrazmendes@gmail.com>
2021-01-08 10:39:35 -0300

    handle api's env variables

--- next.config.js
+++ next.config.js
@@ -4,12 +4,13 @@ module.exports = withImages({
 	esModule: true,
 	images:
 	{
-		// domains: [process.env.API_HOSTNAME],
+		domains: [process.env.API_HOSTNAME, process.env.TMDB_IMG_HOSTNAME],
 		loader: 'imgix',
 		path: ''
 	},
 	publicRuntimeConfig:
 	{
-		
+		apiUrl: process.env.API_URL,
+		apiKey: process.env.API_KEY
 	}
 })
