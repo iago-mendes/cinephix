5c150762f2044168f41ea4b46f876e03b16e9498
Iago <iagobrazmendes@gmail.com>
2021-01-08 10:53:34 -0300

    get home page static info

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,37 @@
+import {GetStaticProps} from 'next'
 import Head from 'next/head'
+import {useEffect} from 'react'
 
 import Container from '../styles/pages/index'
+import api from '../services/api'
 
-const Home: React.FC = () =>
+interface Media
 {
+	id: number
+	image: string
+	title: string
+	overview: string
+	date: string
+}
+
+interface Celebrity
+{
+	id: number
+	image: string
+	name: string
+	knownForDepartment: string
+	knownFor: Media[]
+}
+
+interface HomeProps
+{
+	staticHome: Array<Media | Celebrity>
+}
+
+const Home: React.FC<HomeProps> = ({staticHome}) =>
+{
+	useEffect(() => console.log('[staticHome]', staticHome), [staticHome])
+
 	return (
 		<Container>
 			<Head>
@@ -12,6 +40,16 @@ const Home: React.FC = () =>
 			<h1>Hello world!</h1>
 		</Container>
 	)
+}
+
+export const getStaticProps: GetStaticProps = async ctx =>
+{
+	const {data}:{data: Array<Media | Celebrity>} = await api.get('/home')
+
+	return {
+		props: {staticHome: data},
+		revalidate: 5
+	}
 }
 
 export default Home
