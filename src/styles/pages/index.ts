8091e248fa65212fffe2d9f063b475c8c3ab5fcb
Iago <iagobrazmendes@gmail.com>
2021-01-10 19:38:07 -0300

    remove unused code

--- src/styles/pages/index.ts
+++ src/styles/pages/index.ts
@@ -64,55 +64,6 @@ const Container = styled.div`
 		{
 			width: 50rem;
 		}
-
-		.search
-		{
-			position: absolute;
-			bottom: -2rem;
-			z-index: 1;
-
-			background-color: ${p => p.theme.white};
-			width: 50rem;
-			height: 4rem;
-
-			display: flex;
-			align-items: center;
-			gap: 1rem;
-
-			padding-left: 1rem;
-			padding-right: 2rem;
-			border-radius: 100rem;
-
-			color: ${p => p.theme.blue};
-			box-shadow: 0px 5px 5px rgba(0,0,0,0.5);
-
-			input
-			{
-				width: 100%;
-				height: 90%;
-
-				font-family: Ubuntu;
-				font-weight: 700;
-				font-size: 2rem;
-
-				color: ${p => p.theme.blue};
-				background: none;
-				border: none;
-			}
-		}
-	}
-
-	main
-	{
-		display: grid;
-		grid-auto-rows: calc((40rem - 2rem) * 0.3 * 1.5 + 2rem);
-		grid-template-columns: repeat(auto-fill, minmax(40rem, 1fr));
-		grid-gap: 2rem;
-		align-items: center;
-		justify-items: center;
-		
-		padding: 1rem;
-		margin-top: 2rem;
 	}
 `
 
