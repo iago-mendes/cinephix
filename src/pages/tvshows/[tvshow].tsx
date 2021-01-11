5014c2e9d2d21ac4212eae8bdcdbc8a8b7c0431f
Iago <iagobrazmendes@gmail.com>
2021-01-11 18:23:35 -0300

    create tv show page

---
+++ src/pages/tvshows/[tvshow].tsx
@@ -0,0 +1,178 @@
+import {GetStaticPaths, GetStaticProps} from 'next'
+import Head from 'next/head'
+import {useRouter} from 'next/router'
+import {FiCalendar, FiInfo, FiStar, FiArrowRight} from 'react-icons/fi'
+import {SwiperSlide} from 'swiper/react'
+import Image from 'next/image'
+
+import Container from '../../styles/pages/tvshows/[tvshow]'
+import api from '../../services/api'
+import {Media} from '../../components/MediaCard'
+import Loading from '../../components/Loading'
+import Carousel, {CarouselCard} from '../../components/Carousel'
+import formatDate from '../../utils/formatDate'
+import React from 'react'
+
+interface TvshowDetails
+{
+	id: number
+  image: string
+  title: string
+  startDate: string
+  endDate: string
+  status: string
+  inProduction: boolean
+  rating: number
+  seasonsNumber: number
+  episodesNumber: number
+  overview: string
+  genres: Array<
+	{
+		id: number
+		name: string
+	}>
+	credits:
+	{
+		cast: Array<
+		{
+			id: number
+			name: string
+			image: string
+			character: string
+		}>
+		crew: Array<
+		{
+			id: number
+			name: string
+			image: string
+			department: string
+		}>
+	}
+}
+
+interface TvshowProps
+{
+	tvshow: TvshowDetails
+}
+
+const Tvshow: React.FC<TvshowProps> = ({tvshow}) =>
+{
+	const Router = useRouter()
+
+	if (Router.isFallback)
+		return <Loading />
+
+	return (
+		<Container>
+			<Head>
+				<title>TV Show</title>
+			</Head>
+
+			<main>
+				<div className='img'>
+					<Image src={tvshow.image} width={780} height={1170} layout='responsive'/>
+				</div>
+				<div className='info'>
+					<h1>{tvshow.title}</h1>
+					<div className='details'>
+						<div className='detail'>
+							<FiCalendar size={30} />
+							<span>{formatDate(tvshow.startDate)}</span>
+							<FiArrowRight size={30} />
+							<span>{formatDate(tvshow.endDate)}</span>
+						</div>
+						<div className="group">
+							<div className='detail'>
+								<FiInfo size={30} />
+								<span>{tvshow.status}</span>
+							</div>
+							<div className='detail'>
+								<FiStar size={30} />
+								<span>{tvshow.rating}</span>
+							</div>
+						</div>
+						<div className="group">
+							<div className='detail'>
+								<strong>Nº of seasons:</strong>
+								<span>{tvshow.seasonsNumber}</span>
+							</div>
+							<div className='detail'>
+								<strong>Nº of episodes:</strong>
+								<span>{tvshow.episodesNumber}</span>
+							</div>
+						</div>
+					</div>
+					<p>{tvshow.overview}</p>
+					<ul>
+						{tvshow.genres.map(genre => (
+							<li key={genre.id} >
+								{genre.name}
+							</li>
+						))}
+					</ul>
+				</div>
+			</main>
+
+			<div className='cast'>
+				<span>Cast ({tvshow.credits.cast.length})</span>
+				<Carousel>
+					{tvshow.credits.cast.map((celebrity, index) => (
+						<SwiperSlide key={index} >
+							<CarouselCard
+								image={celebrity.image}
+								primaryDisplay={celebrity.name}
+								secondaryDisplay={celebrity.character}
+								link={`/celebrities/${celebrity.id}`}
+							/>
+						</SwiperSlide>
+					))}
+				</Carousel>
+			</div>
+
+			<div className='crew'>
+				<span>Crew ({tvshow.credits.crew.length})</span>
+				<Carousel>
+					{tvshow.credits.crew.map((celebrity, index) => (
+						<SwiperSlide key={index} >
+							<CarouselCard
+								image={celebrity.image}
+								primaryDisplay={celebrity.name}
+								secondaryDisplay={celebrity.department}
+								link={`/celebrities/${celebrity.id}`}
+							/>
+						</SwiperSlide>
+					))}
+				</Carousel>
+			</div>
+		</Container>
+	)
+}
+
+export const getStaticPaths: GetStaticPaths = async ctx =>
+{
+	const {data: tvshows}:{data: Media[]} = await api.get('tvshows')
+
+	const paths = tvshows.map(tvshow => (
+	{
+		params: {tvshow: String(tvshow.id)}
+	}))
+
+	return {
+		paths,
+		fallback: true
+	}
+}
+
+export const getStaticProps: GetStaticProps = async ctx =>
+{
+	const {tvshow: id} = ctx.params
+
+	const {data: tvshow}:{data: TvshowDetails} = await api.get(`tvshows/${id}`)
+
+	return {
+		props: {tvshow},
+		revalidate: 60
+	}
+}
+
+export default Tvshow
---
+++ src/styles/pages/tvshows/[tvshow].ts
@@ -0,0 +1,150 @@
+import styled from 'styled-components'
+
+const Container = styled.div`
+	main
+	{
+		height: calc(100vh - 5rem);
+		padding: 2rem;
+
+		display: flex;
+		justify-content: space-between;
+
+		.img
+		{
+			width: calc((100vh - 5rem - 4rem) / 1.5);
+			max-width: calc(50% - 5rem);
+
+			img
+			{
+				border-radius: 1rem;
+			}
+		}
+
+		.info
+		{
+			height: 100%;
+			width: 50%;
+			margin-right: 5rem;
+
+			display: flex;
+			flex-direction: column;
+			justify-content: space-around;
+			gap: 2rem;
+
+			color: ${p => p.theme.primary};
+
+			h1
+			{
+				font-family: Ubuntu;
+				font-weight: 700;
+				font-size: 4rem;
+			}
+
+			.details
+			{
+				display: flex;
+				flex-direction: column;
+				align-items: center;
+				gap: 2rem;
+
+				.detail
+				{
+					display: flex;
+					align-items: center;
+					gap: 1rem;
+
+					strong
+					{
+						font-family: Ubuntu;
+						font-weight: 700;
+						font-size: 2rem;
+					}
+
+					span
+					{
+						font-family: Ubuntu;
+						font-weight: 400;
+						font-size: 2rem;
+					}
+				}
+
+				.group
+				{
+					display: flex;
+					align-items: center;
+					justify-content: space-around;
+
+					width: 100%;
+				}
+			}
+
+			p
+			{
+				font-family: Roboto;
+				font-size: 2rem;
+			}
+
+			ul
+			{
+				width: 100%;
+
+				display: grid;
+				grid-auto-rows: 5rem;
+				grid-template-columns: repeat(auto-fill, minmax(20rem, 1fr));
+				grid-gap: 1rem;
+				align-items: center;
+				justify-items: center;
+
+				li
+				{
+					display: flex;
+					align-items: center;
+					justify-content: center;
+
+					text-decoration: none;
+
+					padding: 0.75rem;
+					padding-left: 2rem;
+					padding-right: 2rem;
+
+					border: ${p => p.theme.primary} 1px solid;
+					border-radius: 100rem;
+
+					font-family: Ubuntu;
+					font-weight: 400;
+					font-size: 1.75rem;
+				}
+			}
+		}
+	}
+
+	.cast, .crew
+	{
+		display: flex;
+		flex-direction: column;
+		gap: 3rem;
+
+		padding: 1rem;
+		padding-top: 3rem;
+		padding-bottom: 3rem;
+
+		span
+		{
+			font-family: Roboto;
+			font-weight: 700;
+			font-size: 3rem;
+			color: ${p => p.theme.primary};
+
+			margin-left: 5rem;
+			padding-left: 1rem;
+			border-left: ${p => p.theme.primary} 5px solid;
+		}
+	}
+
+	.cast
+	{
+		background-color: rgba(0,0,0,0.5);
+	}
+`
+
+export default Container
