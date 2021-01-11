3c42b0c30ac4c554e416aa3a18fab256c1fb61e3
Iago <iagobrazmendes@gmail.com>
2021-01-11 17:55:01 -0300

    scroll to top on navigation

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,6 @@
 import {AppProps} from 'next/app'
+import {useRouter} from 'next/router'
+import {useEffect} from 'react'
 import {ThemeProvider} from 'styled-components'
 import SwiperCore, {Navigation} from 'swiper'
 import 'swiper/swiper-bundle.css'
@@ -10,6 +12,13 @@ import theme from '../styles/theme'
 const MyApp: React.FC<AppProps> = ({Component, pageProps}) =>
 {
 	SwiperCore.use([Navigation])
+
+	const {pathname} = useRouter()
+
+	useEffect(() =>
+	{
+		window.scrollTo(0,0)
+	}, [pathname])
 
   return (
     <ThemeProvider theme={theme}>
