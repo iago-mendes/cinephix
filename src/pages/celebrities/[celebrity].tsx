38ecce996942b9cab92c16d2d26fefb060508257
Iago <iagobrazmendes@gmail.com>
2021-01-11 18:57:14 -0300

    create celebrity page

---
+++ src/pages/celebrities/[celebrity].tsx
@@ -0,0 +1,155 @@
+import {GetStaticPaths, GetStaticProps} from 'next'
+import Head from 'next/head'
+import {useRouter} from 'next/router'
+import {FiUser} from 'react-icons/fi'
+import {FaBirthdayCake, FaCity} from 'react-icons/fa'
+import {SwiperSlide} from 'swiper/react'
+import Image from 'next/image'
+
+import Container from '../../styles/pages/celebrities/[celebrity]'
+import api from '../../services/api'
+import {Celebrity as CelebrityList} from '../../components/CelebrityCard'
+import Loading from '../../components/Loading'
+import Carousel, {CarouselCard} from '../../components/Carousel'
+import formatDate from '../../utils/formatDate'
+import React from 'react'
+
+interface CelebrityDetails
+{
+	id: number
+  image: string
+  name: string
+  knownForDepartment: string
+  birthday: string
+  placeOfBirth: string
+  biography: string
+	credits:
+	{
+		cast: Array<
+		{
+			'id': number
+			'title': string
+			'image': string
+			'character': string
+			'overview': string
+			'date': string
+			'type': string
+		}>
+		crew: Array<
+		{
+			'id': number
+			'title': string
+			'image': string
+			'overview': string
+			'date': string
+			'department': string
+			'type': string
+		}>
+	}
+}
+
+interface CelebrityProps
+{
+	celebrity: CelebrityDetails
+}
+
+const Celebrity: React.FC<CelebrityProps> = ({celebrity}) =>
+{
+	const Router = useRouter()
+
+	if (Router.isFallback)
+		return <Loading />
+
+	return (
+		<Container biographyLength={celebrity.biography.length} >
+			<Head>
+				<title>Celebrity</title>
+			</Head>
+
+			<main>
+				<div className='img'>
+					<Image src={celebrity.image} width={780} height={1170} layout='responsive'/>
+				</div>
+				<div className='info'>
+					<h1>{celebrity.name}</h1>
+					<div className='details'>
+						<div className='detail'>
+							<FiUser size={30} />
+							<span>{celebrity.knownForDepartment}</span>
+						</div>
+						<div className='detail'>
+							<FaBirthdayCake size={30} />
+							<span>{formatDate(celebrity.birthday)}</span>
+						</div>
+						<div className='detail'>
+							<FaCity size={30} />
+							<span>{celebrity.placeOfBirth}</span>
+						</div>
+					</div>
+					<p>{celebrity.biography}</p>
+				</div>
+			</main>
+
+			<div className='cast'>
+				<span>Cast ({celebrity.credits.cast.length})</span>
+				<Carousel>
+					{celebrity.credits.cast.map((media, index) => (
+						<SwiperSlide key={index} >
+							<CarouselCard
+								image={media.image}
+								primaryDisplay={media.title}
+								secondaryDisplay={media.character}
+								link={`/celebrities/${media.id}`}
+							/>
+						</SwiperSlide>
+					))}
+				</Carousel>
+			</div>
+
+			<div className='crew'>
+				<span>Crew ({celebrity.credits.crew.length})</span>
+				<Carousel>
+					{celebrity.credits.crew.map((media, index) => (
+						<SwiperSlide key={index} >
+							<CarouselCard
+								image={media.image}
+								primaryDisplay={media.title}
+								secondaryDisplay={media.department}
+								link={`/celebrities/${media.id}`}
+							/>
+						</SwiperSlide>
+					))}
+				</Carousel>
+			</div>
+		</Container>
+	)
+}
+
+export const getStaticPaths: GetStaticPaths = async ctx =>
+{
+	const {data: celebrities}:{data: CelebrityList[]} = await api.get('celebrities')
+
+	const paths = celebrities.map(celebrity => (
+	{
+		params: {celebrity: String(celebrity.id)}
+	}))
+
+	return {
+		paths,
+		fallback: true
+	}
+}
+
+export const getStaticProps: GetStaticProps = async ctx =>
+{
+	const {celebrity: id} = ctx.params
+
+	const {data: celebrity}:{data: CelebrityDetails} = await api.get(`celebrities/${id}`)
+
+	return {
+		props: {celebrity},
+		revalidate: 60
+	}
+}
+
+export default Celebrity
---
+++ src/styles/pages/celebrities/[celebrity].ts
@@ -0,0 +1,114 @@
+import styled from 'styled-components'
+
+interface ContainerProps
+{
+	biographyLength: number
+}
+
+const Container = styled.div<ContainerProps>`
+	main
+	{
+		height: calc(100vh - 5rem);
+		padding: 2rem;
+
+		display: flex;
+		justify-content: space-between;
+
+		.img
+		{
+			width: calc((100vh - 5rem - 4rem) / 1.5);
+			max-width: calc(50% - 5rem);
+
+			img
+			{
+				border-radius: 1rem;
+			}
+		}
+
+		.info
+		{
+			height: 100%;
+			width: 50%;
+			margin-right: 5rem;
+
+			display: flex;
+			flex-direction: column;
+			justify-content: space-around;
+			gap: 2rem;
+
+			color: ${p => p.theme.primary};
+
+			h1
+			{
+				font-family: Ubuntu;
+				font-weight: 700;
+				font-size: 4rem;
+			}
+
+			.details
+			{
+				display: flex;
+				flex-direction: column;
+				align-items: center;
+				gap: 2rem;
+
+				.detail
+				{
+					display: flex;
+					align-items: center;
+					gap: 1rem;
+
+					strong
+					{
+						font-family: Ubuntu;
+						font-weight: 700;
+						font-size: 2rem;
+					}
+
+					span
+					{
+						font-family: Ubuntu;
+						font-weight: 400;
+						font-size: 2rem;
+					}
+				}
+			}
+
+			p
+			{
+				font-family: Roboto;
+				font-size: ${p => p.biographyLength < 1500 ? '1.5rem' : '1.25rem'};
+			}
+		}
+	}
+
+	.cast, .crew
+	{
+		display: flex;
+		flex-direction: column;
+		gap: 3rem;
+
+		padding: 1rem;
+		padding-top: 3rem;
+		padding-bottom: 3rem;
+
+		span
+		{
+			font-family: Roboto;
+			font-weight: 700;
+			font-size: 3rem;
+			color: ${p => p.theme.primary};
+
+			margin-left: 5rem;
+			padding-left: 1rem;
+			border-left: ${p => p.theme.primary} 5px solid;
+		}
+	}
+
+	.cast
+	{
+		background-color: rgba(0,0,0,0.5);
+	}
+`
+
+export default Container
