574ed26a1d1a1a0c4beb5f30083ce0b4784265e9
Iago <iagobrazmendes@gmail.com>
2021-01-23 17:38:29 -0300

    create separate form for user tvshow

--- src/pages/user/tvshows/add/[tvshow].tsx
+++ src/pages/user/tvshows/add/[tvshow].tsx
@@ -1,52 +1,10 @@
 import {GetStaticPaths, GetStaticProps} from 'next'
-import Head from 'next/head'
-import {ChangeEvent, FormEvent, useEffect, useState} from 'react'
-import Select from 'react-select'
-import Image from 'next/image'
-import {useRouter} from 'next/router'
-import {FiCheck, FiX} from 'react-icons/fi'
 
-import Container, {RangeInput} from '../../../../styles/pages/user/tvshows/add/[tvshow]'
 import api from '../../../../services/api'
 import {Media} from '../../../../components/MediaCard'
-import {TvshowDetails} from '../../../tvshows/[tvshow]'
+import TvshowDetails from '../../../../models/tvshow'
 import Loading from '../../../../components/Loading'
-import {selectStyles} from '../../../../styles/global'
-import useUser from '../../../../hooks/useUser'
-import confirmAlert from '../../../../utils/alerts/confirm'
-import errorAlert from '../../../../utils/alerts/error'
-import calcTotalRating from '../../../../utils/getTotalRating'
-
-interface SelectOption
-{
-	label: string
-	value: string
-}
-
-interface Ratings
-{
-	[ratingKey: string]: number
-}
-
-const defaultRatings: Ratings =
-{
-	engagement: -1,
-	consistency: -1,
-	screenplay: -1,
-	acting: -1,
-	cinematography: -1,
-	musicAndSound: -1
-}
-
-const ratingsLabels: {[ratingKey: string]: string} =
-{
-	engagement: 'Engagement',
-	consistency: 'Consistency',
-	screenplay: 'Screenplay',
-	acting: 'Acting',
-	cinematography: 'Cinematography',
-	musicAndSound: 'Music and sound'
-}
+import TvshowForm from '../../../../components/forms/Tvshow'
 
 interface AddTvshowProps
 {
@@ -55,171 +13,16 @@ interface AddTvshowProps
 
 const AddTvshow: React.FC<AddTvshowProps> = ({tvshow}) =>
 {
-	const {query, back, push} = useRouter()
-	const {user} = useUser()
-
-	const [status, setStatus] = useState('')
-	const [venue, setVenue] = useState('')
-	const [ratings, setRatings] = useState<Ratings>(defaultRatings)
-
-	const statusOptions: SelectOption[] = 
-	[
-		{label: 'Watch list', value: 'watchList'},
-		{label: 'Watching', value: 'watching'},
-		{label: 'Waiting', value: 'waiting'},
-		{label: 'Completed', value: 'completed'},
-		{label: 'Stopped', value: 'stopped'},
-		{label: 'Paused', value: 'paused'}
-	]
-
-	const venueOptions: SelectOption[] =
-	[
-		{label: 'Netflix', value: 'Netflix'},
-		{label: 'Prime Video', value: 'Prime Video'},
-		{label: 'Disney+', value: 'Disney+'},
-		{label: 'HBO Max', value: 'HBO Max'},
-		{label: 'Movie Theater', value: 'Movie Theater'},
-		{label: 'Other', value: 'Other'}
-	]
-
-	useEffect(() =>
-	{
-		const {status: statusKey} = query
-
-		if (statusKey)
-			setStatus(String(statusKey))
-	}, [query])
-
-	function handleChangeRating(e: ChangeEvent<HTMLInputElement>, ratingKey: string)
-	{
-		let tmpRatings = {...ratings}
-
-		const rating = Number(e.target.value)
-		if (rating >= 0 && rating <= 10)
-			tmpRatings[ratingKey] = rating
-			
-		setRatings(tmpRatings)
-	}
-
-	function handleSubmit(e: FormEvent)
-	{
-		e.preventDefault()
-
-		const data =
-		{
-			id: tvshow.id,
-			status: status,
-			venue: venue !== '' ? venue : undefined,
-			ratings:
-			{
-				engagement: ratings.engagement >= 0 ? ratings.engagement : undefined,
-				consistency: ratings.consistency >= 0 ? ratings.consistency : undefined,
-				screenplay: ratings.screenplay >= 0 ? ratings.screenplay : undefined,
-				acting: ratings.acting >= 0 ? ratings.acting : undefined,
-				cinematography: ratings.cinematography >= 0 ? ratings.cinematography : undefined,
-				musicAndSound: ratings.musicAndSound >= 0 ? ratings.musicAndSound : undefined
-			}
-		}
-		
-		api.post(`users/${user.email}/tvshows`, data)
-			.then(() =>
-			{
-				confirmAlert(`'${tvshow.title}' was successfully added to your TV shows!`)
-				push('/user/tvshows')
-			})
-			.catch(err =>
-			{
-				errorAlert(err.response.data.message)
-			})
-	}
-
 	if (!tvshow)
 		return <Loading style={{marginTop: 'calc(50vh - 5rem)'}} />
 
 	return (
-		<Container>
-			<Head>
-				<title>Add Tvshow</title>
-			</Head>
-
-			<div className='img'>
-				<Image src={tvshow.image} width={780} height={1170} layout='responsive'/>
-			</div>
-			<div className='info'>
-				<h1>{tvshow.title}</h1>
-				<form onSubmit={handleSubmit} >
-					<div className='selectField'>
-						<label htmlFor='status'>Status</label>
-						<Select
-							id='status'
-							name='status'
-							value={statusOptions.find(({value}) => value === status)}
-							options={statusOptions}
-							onChange={e => setStatus(e.value)}
-							styles={selectStyles}
-							placeholder='Select a status'
-							className='select'
-						/>
-					</div>
-					<div className='selectField'>
-						<label htmlFor='venue'>Venue</label>
-						<Select
-							id='venue'
-							name='venue'
-							value={venueOptions.find(({value}) => value === venue)}
-							options={venueOptions}
-							onChange={e => setVenue(e.value)}
-							styles={selectStyles}
-							placeholder='Select a venue'
-							className='select'
-						/>
-					</div>
-					<div className='rangeFields'>
-						<label>Ratings</label>
-						<div className='rating'>
-							<label>Total:</label>
-							<span>{calcTotalRating(ratings) || 'not rated'}</span>
-						</div>
-						{Object.entries(ratings).map(([ratingKey, value]) => (
-							<div className='rating' key={ratingKey}>
-								<label>{ratingsLabels[ratingKey]}:</label>
-								<div className='group'>
-									{
-										value >= 0
-										? (
-											<input
-												type='number'
-												value={value}
-												onChange={e => handleChangeRating(e, ratingKey)}
-											/>
-										)
-										: (
-											<span>not rated</span>
-										)
-									}
-									<RangeInput
-										type='range'
-										min={0}
-										max={10}
-										value={value >= 0 ? value : 5}
-										onChange={e => handleChangeRating(e, ratingKey)}
-										isUndefined={value < 0}
-									/>
-								</div>
-							</div>
-						))}
-					</div>
-					<div className='buttons'>
-						<button className='cancel' title='Cancel' onClick={back} type='button' >
-							<FiX size={25} />
-						</button>
-						<button className='confirm' title='Confirm' type='submit' >
-							<FiCheck size={25} />
-						</button>
-					</div>
-				</form>
-			</div>
-		</Container>
+		<div>
+			<TvshowForm
+				tvshow={tvshow}
+				method='post'
+			/>
+		</div>
 	)
 }
 
