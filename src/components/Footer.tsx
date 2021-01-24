4e99e4541fdd6ef4448c4d1dc6ec8c2a4fbaded7
Iago <iagobrazmendes@gmail.com>
2021-01-23 21:30:03 -0300

    style footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,19 +1,26 @@
 import Link from 'next/link'
 import Container from '../styles/components/Footer'
 
+import logo from '../assets/logo-name-shadow.svg'
+
 const Footer: React.FC = () =>
 {
 	return (
 		<Container>
-			<div className='links'>
-				<Link href='/' >Home</Link>
-				<Link href='/movies' >Movies</Link>
-				<Link href='/tvshows' >TV shows</Link>
-				<Link href='/celebrities' >Celebrities</Link>
-				<Link href='/about' >About</Link>
+			<div className='logo'>
+				<img src={logo} alt='Cinephix'/>
 			</div>
-			<div className='attribution'>
-				<Link href='/about/data'>Media data</Link> from <a href='https://www.themoviedb.org/'>TMDb</a>.
+			<div className='info'>
+				<div className='links'>
+					<Link href='/' >Home</Link>
+					<Link href='/movies' >Movies</Link>
+					<Link href='/tvshows' >TV shows</Link>
+					<Link href='/celebrities' >Celebrities</Link>
+					<Link href='/about' >About</Link>
+				</div>
+				<div className='attribution'>
+					<Link href='/about/data'>Media data</Link> from <a target='_blank' href='https://www.themoviedb.org/'>TMDb</a>.
+				</div>
 			</div>
 		</Container>
 	)
--- src/styles/components/Footer.ts
+++ src/styles/components/Footer.ts
@@ -1,7 +1,86 @@
 import styled from 'styled-components'
 
 const Container = styled.footer`
-	
+	background-color: ${p => p.theme.black};
+	padding: 3rem;
+
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+
+	.logo
+	{
+		width: 25%;
+
+		display: flex;
+		align-items: center;
+		justify-content: center;
+
+		img
+		{
+			width: 100%;
+		}
+	}
+
+	.info
+	{
+		width: 70%;
+		height: 100%;
+
+		display: flex;
+		flex-direction: column;
+		align-items: flex-end;
+		gap: 2rem;
+
+		.links
+		{
+			width: 100%;
+
+			display: flex;
+			align-items: center;
+			justify-content: space-between;
+
+			a
+			{
+				font-family: Ubuntu;
+				font-weight: 700;
+				font-size: 2rem;
+
+				color: ${p => p.theme.primary};
+				text-decoration: none;
+
+				display: inline-block;
+
+				::after
+				{
+					content: '';
+					width: 0px;
+					height: 2px;
+					display: block;
+					background: ${p => p.theme.primary};
+					transition: 0.25s;
+				}
+
+				:hover::after
+				{
+					width: 100%;
+				}
+			}
+		}
+
+		.attribution
+		{
+			font-family: Roboto;
+			font-size: 1.25rem;
+			
+			color: ${p => p.theme.gray};
+
+			a
+			{
+				color: ${p => p.theme.gray};
+			}
+		}
+	}
 `
 
 export default Container
