4e5835eccd3832ca42c70f2f7a0b4cd68a6296c5
Iago <iagobrazmendes@gmail.com>
2021-01-25 22:04:07 -0300

    complete user page

--- src/pages/user/index.tsx
+++ src/pages/user/index.tsx
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import {useEffect, useState} from 'react'
+import {HiOutlineUserRemove} from 'react-icons/hi'
+import {FiArrowRight} from 'react-icons/fi'
 
 import Container from '../../styles/pages/user/index'
 import Loading from '../../components/Loading'
@@ -8,6 +10,7 @@ import useUser from '../../hooks/useUser'
 import UserInfo, {defaultUser} from '../../models/user'
 import api from '../../services/api'
 import errorAlert from '../../utils/alerts/error'
+import Link from 'next/link'
 
 const User: React.FC = () =>
 {
@@ -29,6 +32,17 @@ const User: React.FC = () =>
 				})
 	}, [user])
 
+	function formatDate(unformatedDate: string)
+	{
+		const months =
+		[
+			'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'
+		]
+
+		const [year, month, day] = unformatedDate.split('T')[0].split('-').map(s => Number(s))
+		return `${months[month]} ${day}, ${year}`
+	}
+
 	if (!user)
 		return <Loading style={{marginTop: 'calc(50vh - 5rem)'}} />
 
@@ -42,12 +56,35 @@ const User: React.FC = () =>
 				<div className='img'>
 					<Image src={user.image} width={100} height={100} layout='responsive'/>
 				</div>
-				<div className="group">
+				<div className='group'>
 					<h1>{user.name}</h1>
 					<h2>{user.email}</h2>
-					<p>Member since </p>
+					<p>Member since <strong>{formatDate(userInfo.joinedAt)}</strong></p>
 				</div>
+				<button className='delete'>
+					<HiOutlineUserRemove size={25} />
+					<span>Delete account</span>
+				</button>
 			</main>
+
+			<div className="links">
+				<Link href='/user/tvshows'>
+					<a>
+						<span>
+							My TV shows ({userInfo.tvshows.length})
+							<FiArrowRight size={40} />
+						</span>
+					</a>
+				</Link>
+				<Link href='/user/movies'>
+					<a>
+						<span>
+							My movies ({userInfo.movies.length})
+							<FiArrowRight size={40} />
+						</span>
+					</a>
+				</Link>
+			</div>
 		</Container>
 	)
 }
--- src/styles/pages/user/index.ts
+++ src/styles/pages/user/index.ts
@@ -3,9 +3,122 @@ import styled from 'styled-components'
 const Container = styled.div`
 	main
 	{
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+
+		padding: 2rem;
+
+		border-bottom: ${p => p.theme.primary} 2px solid;
+
 		.img
 		{
 			width: 25rem;
+
+			img
+			{
+				border-radius: 2rem;
+			}
+		}
+
+		.group
+		{
+			display: flex;
+			flex-direction: column;
+			gap: 1rem;
+
+			color: ${p => p.theme.primary};
+			font-family: Roboto;
+
+			h1
+			{
+				font-size: 4rem;
+			}
+
+			h2
+			{
+				font-size: 3rem;
+			}
+
+			p
+			{
+				font-size: 2rem;
+			}
+		}
+
+		.delete
+		{
+			background: none;
+			border: ${p => p.theme.delete} 2px solid;
+			border-radius: 1rem;
+
+			display: flex;
+			align-items: center;
+			gap: 1rem;
+
+			padding: 1rem;
+			color: ${p => p.theme.delete};
+
+			cursor: pointer;
+			transition: 0.25s;
+
+			:hover
+			{
+				background-color: ${p => p.theme.delete};
+				color: ${p => p.theme.background};
+
+				transform: scale(1.1);
+			}
+
+			span
+			{
+				font-family: Ubuntu;
+				font-weight: 700;
+				font-size: 2rem;
+			}
+		}
+	}
+	
+	.links
+	{
+		display: flex;
+		align-items: center;
+		justify-content: space-around;
+
+		padding: 5rem;
+
+		a
+		{
+			text-decoration: none;
+			font-family: Ubuntu;
+			font-weight: 700;
+			font-size: 3rem;
+
+			color: ${p => p.theme.primary};
+			display: inline-block;
+
+			::after
+			{
+				content: '';
+				width: 0px;
+				height: 5px;
+				display: block;
+				background: ${p => p.theme.primary};
+				transition: 0.25s;
+			}
+
+			:hover::after
+			{
+				width: 100%;
+			}
+
+			span
+			{
+				display: flex;
+				align-items: center;
+				justify-content: center;
+				gap: 1rem;
+			}
 		}
 	}
 `
