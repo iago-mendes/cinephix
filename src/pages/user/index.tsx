f57fb07727db3da6aec9f43671605852eab1ab9d
Iago <iagobrazmendes@gmail.com>
2021-01-25 22:13:52 -0300

    handle delete account

--- src/pages/user/index.tsx
+++ src/pages/user/index.tsx
@@ -4,6 +4,9 @@ import {useEffect, useState} from 'react'
 import {HiOutlineUserRemove} from 'react-icons/hi'
 import {FiArrowRight} from 'react-icons/fi'
 import Link from 'next/link'
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
+import {signOut} from 'next-auth/client'
 
 import Container from '../../styles/pages/user/index'
 import Loading from '../../components/Loading'
@@ -11,6 +14,9 @@ import useUser from '../../hooks/useUser'
 import UserInfo, {defaultUser} from '../../models/user'
 import api from '../../services/api'
 import errorAlert from '../../utils/alerts/error'
+import confirmAlert from '../../utils/alerts/confirm'
+
+const MySwal = withReactContent(Swal)
 
 const User: React.FC = () =>
 {
@@ -43,6 +49,34 @@ const User: React.FC = () =>
 		return `${months[month]} ${day}, ${year}`
 	}
 
+	function handleDelete()
+	{
+		MySwal.fire(
+			{
+				icon: 'question',
+				title: 'Are you sure?',
+				text: `If you continue, your account and all your information (TV shows and movies) will be deleted!`,
+				showCancelButton: true,
+				confirmButtonText: 'Continue'
+			})
+			.then(res =>
+			{
+				if (res.isConfirmed)
+				{
+					api.delete(`users/${userInfo.email}`)
+					.then(() =>
+					{
+						confirmAlert(`Your account was successfully deleted!`)
+						signOut()
+					})
+					.catch(err =>
+					{
+						errorAlert(err.response.data.message)
+					})
+				}
+			})
+	}
+
 	if (!user)
 		return <Loading style={{marginTop: 'calc(50vh - 5rem)'}} />
 
@@ -61,7 +95,7 @@ const User: React.FC = () =>
 					<h2>{user.email}</h2>
 					<p>Member since <strong>{formatDate(userInfo.joinedAt)}</strong></p>
 				</div>
-				<button className='delete'>
+				<button className='delete' onClick={handleDelete} >
 					<HiOutlineUserRemove size={25} />
 					<span>Delete account</span>
 				</button>
