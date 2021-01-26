b096832fbe56725b3e6ce2652bbfeec4049e9402
Iago <iagobrazmendes@gmail.com>
2021-01-26 17:04:31 -0300

    create dimensions hook

---
+++ src/hooks/useDimensions.ts
@@ -0,0 +1,25 @@
+import {useEffect, useState} from 'react'
+
+function useDimensions()
+{
+	const [width, setWidth] = useState(1500)
+	const [height, setHeight] = useState(800)
+	
+	useEffect(() =>
+	{
+		updateDimensions()
+		window.addEventListener('resize', updateDimensions)
+
+		return () => window.removeEventListener('resize', updateDimensions)
+	}, [])
+
+	function updateDimensions()
+	{
+		setWidth(window.innerWidth)
+		setHeight(window.innerHeight)
+	}
+
+	return {width, height}
+}
+
+export default useDimensions
