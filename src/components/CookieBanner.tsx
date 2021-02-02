d271a69ead262db47dafbbf589f6299ca873a889
Iago <iagobrazmendes@gmail.com>
2021-02-02 18:12:27 -0300

    style cookie banner

--- src/components/CookieBanner.tsx
+++ src/components/CookieBanner.tsx
@@ -1,18 +1,92 @@
 import Link from 'next/link'
+import {CSSProperties} from 'react'
 import Banner from 'react-cookie-banner'
+
+import useDimensions from '../hooks/useDimensions'
 
 const CookieBanner: React.FC = () =>
 {
+	const {width} = useDimensions()
+
+	const bannerDesktopStyle: CSSProperties =
+	{
+		position: 'fixed',
+		bottom: '2rem',
+		right: '2rem',
+
+		width: '50vw',
+		height: '20vh',
+		padding: '1rem',
+
+		backgroundColor: '#26070B',
+		boxShadow: '0px 0px 5px #000',
+		borderRadius: '1rem',
+
+		display: 'flex',
+		alignItems: 'flex-end',
+		justifyContent: 'space-between',
+		flexDirection: 'column',
+	}
+
+	const bannerMobileStyle: CSSProperties =
+	{
+		position: 'fixed',
+		bottom: '2rem',
+		right: '2rem',
+
+		width: 'calc(100vw - 4rem)',
+		height: '20vh',
+		padding: '1rem',
+
+		backgroundColor: '#26070B',
+		boxShadow: '0px 0px 5px #000',
+		borderRadius: '1rem',
+
+		display: 'flex',
+		alignItems: 'flex-end',
+		justifyContent: 'space-between',
+		flexDirection: 'column',
+	}
+
 	return (
 		<Banner
-			message='This site uses cookies to provide you with a great user experience. By using Cinephix, you accept our policies.'
+			message='This site uses cookies to provide you with a great user experience. By using Cinephix, you accept our policies. '
 			cookie='user-has-accepted-cookies'
 			buttonMessage='Ok'
 			link={(
 				<Link href='/about/privacy-policy' >
-					Privacy Policy
+					<a style={{color: '#7B7B7B'}} >
+						Privacy Policy
+					</a>
 				</Link>
 			)}
+			className='cookie-banner'
+			styles={{
+				banner: width <= 700 ? bannerMobileStyle : bannerDesktopStyle,
+				message:
+				{
+					fontFamily: 'Ubuntu',
+					fontWeight: 700,
+					fontSize: '1.75rem',
+					lineHeight: '2rem',
+					color: '#FF8A00',
+
+					width: '100%',
+					textAlign: 'left',
+				},
+				button:
+				{
+					position: 'static',
+					backgroundColor: '#FF8A00',
+					color: '#26070B',
+
+					borderRadius: '1rem',
+
+					fontFamily: 'Ubuntu',
+					fontWeight: 700,
+					fontSize: '1.75rem',
+				}
+			}}
 		/>
 	)
 }
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -77,6 +77,16 @@ export default createGlobalStyle`
 			font-family: Ubuntu !important;
 		}
 	}
+
+	.cookie-banner.react-cookie-banner
+	{
+		/* background-color: ${p => p.theme.primary}80; */
+		background-color: green;
+	
+		.cookie-message {
+			font-weight: 400;
+		}
+	}
 `
 
 export const modalStyle: Modal.Styles =
