1f99fd70429ee073960c69eb38d0cff958a84bdb
Iago <iagobrazmendes@gmail.com>
2021-02-09 10:31:44 -0300

    create and style banner

--- src/components/CookieBanner.tsx
+++ src/components/CookieBanner.tsx
@@ -1,19 +1,24 @@
-import {useEffect} from 'react'
+import {useEffect, useState} from 'react'
 import Swal from 'sweetalert2'
 import withReactContent from 'sweetalert2-react-content'
 import {useRouter} from 'next/router'
 
+import Container from '../styles/components/CookieBanner'
+import { FiX } from 'react-icons/fi'
+
 const MySwal = withReactContent(Swal)
 
-const CookieBanner: React.FC= () =>
+const CookieBanner: React.FC = () =>
 {
 	const {push, pathname} = useRouter()
+
+	const [show, setShow] = useState(true)
 
 	useEffect(() =>
 	{
 		const cookieConsent = localStorage.getItem('user-has-accepted-cookies')
 		if (!cookieConsent && pathname !== '/about/privacy-policy')
-			setTimeout(showBanner, 5000)
+			setTimeout(() => setShow(true), 5000)
 	}, [pathname])
 
 	function showBanner()
@@ -35,7 +40,24 @@ const CookieBanner: React.FC= () =>
 			})
 	}
 
-	return null
+	if (!show)
+		return null
+	
+	return (
+		<Container>
+			<button id="close">
+				<FiX size={20} />
+			</button>
+			<div className='message'>
+				<h2>Cookies</h2>
+				<p>This site uses cookies to provide you with a great user experience. By using Cinephix, you accept our policies.</p>
+			</div>
+			<div className='buttons'>
+				<button className='accept'>Ok</button>
+				<button>See Privacy Policy</button>
+			</div>
+		</Container>
+	)
 }
 
 export default CookieBanner
---
+++ src/styles/components/CookieBanner.ts
@@ -0,0 +1,83 @@
+import styled from 'styled-components'
+
+const Container = styled.div`
+	position: fixed;
+	right: 1rem;
+	bottom: 1rem;
+	z-index: 1;
+
+	width: 50vw;
+	padding: 1rem;
+	border-radius: 1rem;
+
+	background-color: ${p => p.theme.background};
+	box-shadow: 0px 0px 10px #000;
+
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+
+	.message
+	{
+		color: ${p => p.theme.primary};
+
+		width: 75%;
+
+		h2
+		{
+			font-family: Ubuntu;
+			font-size: 2rem;
+		}
+
+		p
+		{
+			font-family: Roboto;
+			font-size: 1.75rem;
+		}
+	}
+
+	.buttons
+	{
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		gap: 1rem;
+
+		button
+		{
+			font-family: Ubuntu;
+			font-size: 1.75rem;
+
+			background-color: ${p => p.theme.gray};
+			color: ${p => p.theme.background};
+
+			border: none;
+			border-radius: 0.5rem;
+			padding: 0.5rem;
+		}
+
+		.accept
+		{
+			background-color: ${p => p.theme.primary};
+			padding-left: 2rem;
+			padding-right: 2rem;
+
+			font-weight: 700;
+		}
+	}
+
+	#close
+	{
+		position: absolute;
+		right: 0.5rem;
+		top: 0.5rem;
+
+		border: none;
+		background: none;
+		color: ${p => p.theme.primary};
+
+		cursor: pointer;
+	}
+`
+
+export default Container
