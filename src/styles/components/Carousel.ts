1a088ef0c70aca2377920e53dc6c742337c2493b
Iago <iagobrazmendes@gmail.com>
2021-02-20 12:59:56 -0300

    make responsive

--- src/styles/components/Carousel.ts
+++ src/styles/components/Carousel.ts
@@ -3,9 +3,6 @@ import styled from 'styled-components'
 const Container = styled.div`
 	.swiper
 	{
-		padding-left: 5rem;
-		padding-right: 5rem;
-
 		.swiper-slide
 		{
 			display: flex;
@@ -15,35 +12,45 @@ const Container = styled.div`
 
 		.swiper-button-prev, .swiper-button-next
 		{
-			width: 3.5rem;
-			height: 6rem;
-			border-radius: 100rem;
-
 			color: ${p => p.theme.primary};
 			transition: 0.25s;
 
 			:hover
 			{
-				color: ${p => p.theme.background};
-				background-color: ${p => p.theme.primary};
-
-				box-shadow: 0px 0px 10px rgba(0,0,0,0.5);
+				transform: scale(1.1);
 			}
 		}
 	}
 
-	@media(max-width: 600px)
+	@media(min-width: 600px)
 	{
-		.swiper .swiper-slide .card
+		.swiper
 		{
-			width: 25rem;
-			height: calc((25rem - 2rem) * 0.3 * 1.5 + 2rem);
+			padding-left: 5rem;
+			padding-right: 5rem;
 
-			.info
+			.swiper-slide
 			{
-				h1
+				display: flex;
+				align-items: center;
+				justify-content: center;
+			}
+
+			.swiper-button-prev, .swiper-button-next
+			{
+				width: 3.5rem;
+				height: 6rem;
+				border-radius: 100rem;
+
+				color: ${p => p.theme.primary};
+				transition: 0.25s;
+
+				:hover
 				{
-					font-size: 2rem;
+					color: ${p => p.theme.background};
+					background-color: ${p => p.theme.primary};
+
+					box-shadow: 0px 0px 10px rgba(0,0,0,0.5);
 				}
 			}
 		}
