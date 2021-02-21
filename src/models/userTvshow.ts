2c86bfa1bdfa89d4b49db8079242cbaf9aca61b8
Iago <iagobrazmendes@gmail.com>
2021-02-21 15:31:50 -0300

    write status info

--- src/models/userTvshow.ts
+++ src/models/userTvshow.ts
@@ -20,12 +20,12 @@ export const defaultUserTvshow: UserTvshow =
 
 export const statusInfo: {[statusKey: string]: string} =
 {
-	watchList: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quam est, fermentum ut felis vel, dictum iaculis augue.',
-	watching: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quam est, fermentum ut felis vel, dictum iaculis augue.',
-	waiting: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quam est, fermentum ut felis vel, dictum iaculis augue.',
-	completed: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quam est, fermentum ut felis vel, dictum iaculis augue.',
-	stopped: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quam est, fermentum ut felis vel, dictum iaculis augue.',
-	paused: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quam est, fermentum ut felis vel, dictum iaculis augue.'
+	watchList: 'This status is dedicated to TV shows you want to watch but haven\'t done it yet.',
+	watching: 'This status is dedicated to TV shows you are currently watching.',
+	waiting: 'This status is dedicated to TV shows you have already started watching but haven\'t finished. For example, if you are waiting for more seasons of a show, this is the right place to put it.',
+	completed: 'This status is dedicated to TV shows you have already finished watching and there are no more seasons in production (this applies to cancelled shows as well).',
+	stopped: 'This status is dedicated to TV shows you have already started watching but stopped before the end of it. This could be because you got bored, the series quality decreased, etc..',
+	paused: 'This status is dedicated to TV shows you have already started watching, stopped before its end, AND you plan on finishing it some day.'
 }
 
 export default UserTvshow
