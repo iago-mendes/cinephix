bba72ac7cb4afb0cca521f47ed9902d7e177290c
Iago <iagobrazmendes@gmail.com>
2021-02-21 16:55:56 -0300

    style sort modal

--- src/components/modals/Sort.tsx
+++ src/components/modals/Sort.tsx
@@ -1,8 +1,10 @@
 import {useState} from 'react'
 import {BiSort} from 'react-icons/bi'
-import useClickOutside from '../../hooks/useClickOutside'
+import {motion} from 'framer-motion'
+import {FiX} from 'react-icons/fi'
 
 import Container from '../../styles/components/modals/Sort'
+import useClickOutside from '../../hooks/useClickOutside'
 
 const SortModal: React.FC = () =>
 {
@@ -13,9 +15,59 @@ const SortModal: React.FC = () =>
 		<Container ref={ref} >
 			<button
 				title='Sort TV shows'
+				onClick={() => setShowOptions(true)}
 			>
 				<BiSort size={20} />
 			</button>
+
+			<motion.div
+				initial={false}
+				transition={{duration: 0.25}}
+				animate={showOptions ? 'open' : 'closed'}
+				variants=
+					{{
+						open:
+						{
+							height: 'fit-content',
+							width: 'fit-content',
+							opacity: 1,
+						},
+						closed:
+						{
+							height: 0,
+							width: 0,
+							opacity: 0,
+						}
+					}}
+				style=
+					{{
+						position: 'absolute',
+						right: 0,
+						top: '3rem',
+						zIndex: 100,
+
+						overflow: 'hidden',
+						direction: 'rtl',
+					}}
+			>
+				<div className='options' >
+					<header>
+						<div className='group'>
+							<h3>Sort by...</h3>
+							<button className='close' onClick={() => setShowOptions(false)} >
+								<FiX size={20} />
+							</button>
+						</div>
+					</header>
+					<ul>
+						<li>Title (alphabetically)</li>
+						<li>Release date (newest first)</li>
+						<li>Release date (oldest first)</li>
+						<li>My rating (highest first)</li>
+						<li>My rating (lowest first)</li>
+					</ul>
+				</div>
+			</motion.div>
 		</Container>
 	)
 }
--- src/styles/components/modals/Sort.ts
+++ src/styles/components/modals/Sort.ts
@@ -1,7 +1,65 @@
 import styled from 'styled-components'
 
 const Container = styled.div`
-	
+	position: relative;
+
+	.options
+	{
+		background-color: ${p => p.theme.black};
+		border: ${p => p.theme.primary}40 1px solid;
+
+		width: 20rem;
+		border-radius: 0.5rem;
+
+		direction: ltr;
+		
+		header
+		{
+			::after
+			{
+				margin-top: 0.5rem !important;
+			}
+
+			.group
+			{
+				margin-top: 0.5rem;
+
+				h3
+				{
+					font-family: Ubuntu;
+					font-size: 1.75rem;
+					color: ${p => p.theme.primary};
+				}
+			}
+		}
+
+		ul
+		{
+			padding: 1rem;
+			list-style: none;
+
+			li
+			{
+				font-family: Roboto;
+				font-size: 1.5rem;
+				color: ${p => p.theme.primary};
+
+				width: 100%;
+				height: 3rem;
+
+				display: flex;
+				align-items: center;
+
+				cursor: pointer;
+				transition: 0.25s;
+
+				:hover
+				{
+					background-color: ${p => p.theme.gray}40;
+				}
+			}
+		}
+	}
 `
 
 export default Container
