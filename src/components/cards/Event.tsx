fd87513336334a3f40988ce06927e9c244eb7939
Iago <iagobrazmendes@gmail.com>
2021-03-11 10:58:04 -0300

    style event card

--- src/components/cards/Event.tsx
+++ src/components/cards/Event.tsx
@@ -1,5 +1,6 @@
 import Container from '../../styles/components/cards/Event'
 import {EventListed} from '../../models/event'
+import truncateText from '../../utils/truncateText'
 
 interface EventCardProps
 {
@@ -11,14 +12,14 @@ const EventCard: React.FC<EventCardProps> = ({event}) =>
 	return (
 		<Container>
 			<svg width={50} height={50}>
-				<circle cx={25} cy={25} r={25} fill={event.color} />
+				<rect width={50} height={50} fill={event.color} />
 			</svg>
 			<div className='info'>
 				<span className='name'>
 					{event.name}
 				</span>
 				<p className='description'>
-					{event.description}
+					{truncateText(event.description, 100)}
 				</p>
 			</div>
 		</Container>
--- src/styles/components/cards/Event.ts
+++ src/styles/components/cards/Event.ts
@@ -1,7 +1,57 @@
 import styled from 'styled-components'
 
 const Container = styled.div`
-	
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+
+	background-color: ${p => p.theme.primary};
+	padding: 1rem;
+	width: 30rem;
+	height: 15rem;
+
+	border-radius: 1rem;
+
+	cursor: pointer;
+	transition: 0.25s;
+
+	:hover
+	{
+		border-radius: 0;
+		background-color: ${p => p.theme.primary}bf;
+
+		svg
+		{
+			border-radius: 0;
+		}
+	}
+
+	svg
+	{
+		border-radius: 2.5rem;
+		transition: 0.25s;
+	}
+
+	.info
+	{
+		width: 75%;
+
+		display: flex;
+		flex-direction: column;
+		gap: 1.5rem;
+
+		.name
+		{
+			font-family: Ubuntu;
+			font-size: 2.5rem;
+			font-weight: 700;
+		}
+
+		.description
+		{
+			font-size: 1.5rem;
+		}
+	}
 `
 
 export default Container
