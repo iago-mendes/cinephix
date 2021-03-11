a3a36a702458d7650b702527847e77b621c8e904
Iago <iagobrazmendes@gmail.com>
2021-03-11 11:01:45 -0300

    add link

--- src/components/cards/Event.tsx
+++ src/components/cards/Event.tsx
@@ -1,6 +1,7 @@
 import Container from '../../styles/components/cards/Event'
 import {EventListed} from '../../models/event'
 import truncateText from '../../utils/truncateText'
+import Link from 'next/link'
 
 interface EventCardProps
 {
@@ -9,20 +10,27 @@ interface EventCardProps
 
 const EventCard: React.FC<EventCardProps> = ({event}) =>
 {
+	const link = `/events/${event.id}`
+
 	return (
-		<Container>
-			<svg width={50} height={50}>
-				<rect width={50} height={50} fill={event.color} />
-			</svg>
-			<div className='info'>
-				<span className='name'>
-					{event.name}
-				</span>
-				<p className='description'>
-					{truncateText(event.description, 100)}
-				</p>
-			</div>
-		</Container>
+		<Link href={link} >
+			<Container
+				as='a'
+				href={link}
+			>
+				<svg width={50} height={50}>
+					<rect width={50} height={50} fill={event.color} />
+				</svg>
+				<div className='info'>
+					<span className='name'>
+						{event.name}
+					</span>
+					<p className='description'>
+						{truncateText(event.description, 100)}
+					</p>
+				</div>
+			</Container>
+		</Link>
 	)
 }
 
