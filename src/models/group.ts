1b1689815e9231ef5d04e9feb92e9749576a2c0c
Iago <iagobrazmendes@gmail.com>
2021-03-13 19:42:03 -0300

    add group interface

--- src/models/group.ts
+++ src/models/group.ts
@@ -28,3 +28,90 @@ export interface GroupListed
 	event: EventListed
 	description: string
 }
+
+interface Group
+{
+	urlId: string
+	banner: string
+	nickname: string
+	description: string
+	participants: Array<
+	{
+		email: string
+		isOwner: true
+		predictions: Array<
+		{
+			category:
+			{
+				id: string
+				name: string
+				description: string
+				type: string
+			}
+			guess:
+			{
+				id: 484718
+				image: string
+				title: string
+				overview: string
+				date: string
+				type: string
+			}
+		}>
+	}>
+	event:
+	{
+		id: string
+		name: string
+		color: string
+		description: string
+		categories: Array<
+		{
+			id: string
+			name: string
+			description: string
+			type: string
+			media: Array<
+			{
+				id: 484718
+				image: string
+				title: string
+				overview: string
+				date: string
+				type: string
+				participants: Array<
+				{
+					email: string
+					image: string
+					name: string
+				}>
+			}>
+			celebrities: Array<
+			{
+				celebrity:
+				{
+					id: 1753914
+					image: string
+					name: string
+				}
+				media:
+				{
+					id: 583689
+					image: string
+					title: string
+					overview: string
+					date: string
+					type: string
+				}
+				participants: Array<
+				{
+					email: string
+					image: string
+					name: string
+				}>
+			}>
+		}>
+	}
+}
+
+export default Group
