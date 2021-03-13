3e453277bca293e2a3886619d8351fc01062fcc5
Iago <iagobrazmendes@gmail.com>
2021-03-13 11:32:02 -0300

    create event field

--- src/components/forms/Group.tsx
+++ src/components/forms/Group.tsx
@@ -1,9 +1,15 @@
 import {useEffect, useState} from 'react'
+import Select from 'react-select'
+import {useRouter} from 'next/router'
+import Image from 'next/image'
 
 import Container from '../../styles/components/forms/Group'
 import {GroupRaw} from '../../models/group'
-import getBanner, { chooseRandomBanner } from '../../utils/getBanner'
-import Image from 'next/image'
+import getBanner, {chooseRandomBanner} from '../../utils/getBanner'
+import {SelectOption} from '../../models'
+import {selectStyles} from '../../styles/global'
+import api from '../../services/api'
+import {EventListed} from '../../models/event'
 
 interface GroupFormProps
 {
@@ -13,12 +19,16 @@ interface GroupFormProps
 
 const GroupForm: React.FC<GroupFormProps> = ({method}) =>
 {
+	const {query} = useRouter()
+
+	const [banner, setBanner] = useState('')
 	const [nickname, setNickname] = useState('')
 	const [urlId, setUrlId] = useState('')
-	const [banner, setBanner] = useState('')
 	const [event, setEvent] = useState('')
 	const [description, setDescription] = useState('')
 	const [participantEmails, setParticipantEmails] = useState<string[]>([])
+
+	const [eventOptions, setEventOptions] = useState<SelectOption[]>([])
 
 	useEffect(() =>
 	{
@@ -27,7 +37,30 @@ const GroupForm: React.FC<GroupFormProps> = ({method}) =>
 			const randomBanner = chooseRandomBanner()
 			setBanner(randomBanner)
 		}
+
+		api.get('events').then(({data}:{data: EventListed[]}) =>
+		{
+			const tmpEventOptions: SelectOption[] = data.map(event => (
+				{
+					label: event.name,
+					value: event.id
+				}))
+			
+			setEventOptions(tmpEventOptions)
+		})
 	}, [])
+
+	useEffect(() =>
+	{
+		const {event: routeEvent} = query
+		if (routeEvent)
+		{
+			const tmpEvent = eventOptions.find(event => event.value === String(routeEvent))
+
+			if (tmpEvent)
+				setEvent(tmpEvent.value)
+		}
+	}, [query, eventOptions])
 
 	return (
 		<Container>
@@ -39,7 +72,7 @@ const GroupForm: React.FC<GroupFormProps> = ({method}) =>
 					Change banner
 				</button>
 			</header>
-			
+
 			{/* nickname */}
 			<div className='text field'>
 				<label htmlFor='nickname'>Nickname</label>
@@ -70,6 +103,21 @@ const GroupForm: React.FC<GroupFormProps> = ({method}) =>
 					id='description'
 					value={description}
 					onChange={e => setDescription(e.target.value)}
+				/>
+			</div>
+			{/* event */}
+			<div className='select field'>
+				<label htmlFor='event'>Event</label>
+				<Select
+					id='event'
+					name='event'
+					value={eventOptions.find(({value}) => value === event)}
+					options={eventOptions}
+					onChange={e => setEvent(e.value)}
+					styles={selectStyles}
+					placeholder='Select a event'
+					className='select'
+					isSearchable={false}
 				/>
 			</div>
 		</Container>
