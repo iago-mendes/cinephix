65f42e1d88322fb204e639ad8c626241a507bcdb
Iago <iagobrazmendes@gmail.com>
2021-03-14 17:37:13 -0300

    show and style selec banner modal

--- src/components/modals/SelectBanner.tsx
+++ src/components/modals/SelectBanner.tsx
@@ -1,4 +1,9 @@
+import Image from 'next/image'
+import {FiCheck, FiX} from 'react-icons/fi'
+
+import banners from '../../../db/banners.json'
 import Container from '../../styles/components/modals/SelectBanner'
+import getBanner from '../../utils/getBanner'
 import ModalContainer from './Container'
 
 interface SelectBannerModalProps
@@ -11,8 +16,11 @@ interface SelectBannerModalProps
 }
 
 const SelectBannerModal: React.FC<SelectBannerModalProps> =
-({isOpen, setIsOpen, banner, setBanner}) =>
+({isOpen, setIsOpen, banner: selected, setBanner}) =>
 {
+	function handleSubmit()
+	{}
+
 	return (
 		<ModalContainer
 			isOpen={isOpen}
@@ -24,6 +32,17 @@ const SelectBannerModal: React.FC<SelectBannerModalProps> =
 				</h1>
 
 				<div className='grid' >
+					{banners.map((banner, index) => (
+						<div
+							className={banner.path === selected ? 'selected banner' : 'banner'}
+							key={index}
+						>
+							<Image src={getBanner(banner.path)} width={1500} height={750} layout='responsive' quality={25} />
+							<span>
+								{banner.alt}
+							</span>
+						</div>
+					))}
 				</div>
 			</Container>
 		</ModalContainer>
--- src/styles/components/modals/SelectBanner.ts
+++ src/styles/components/modals/SelectBanner.ts
@@ -1,7 +1,67 @@
 import styled from 'styled-components'
 
 const Container = styled.div`
-	
+	color: ${p => p.theme.primary};
+
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+	gap: 2rem;
+
+	width: 100%;
+	min-height: 100%;
+	padding: 2rem 1rem;
+
+	h1
+	{
+		font-family: Ubuntu;
+		font-size: 2.5rem;
+	}
+
+	.grid
+	{
+		width: 100%;
+
+		display: grid;
+		grid-template-columns: repeat(auto-fill, minmax(25rem, 1fr));
+		grid-gap: 2rem;
+		align-items: center;
+		justify-items: center;
+
+		.banner
+		{
+			width: 25rem;
+			position: relative;
+
+			cursor: pointer;
+			transition: 0.25s;
+
+			:hover
+			{
+				transform: scale(1.1);
+			}
+
+			span
+			{
+				position: absolute;
+				right: 0;
+				bottom: 0;
+
+				background-color: ${p => p.theme.background}bf;
+				padding: 1rem 2rem;
+
+
+				font-family: Ubuntu;
+				font-weight: 700;
+				font-size: 2rem;
+			}
+		}
+
+		.selected
+		{
+			border: ${p => p.theme.primary} 5px solid;
+		}
+	}
 `
 
 export default Container
