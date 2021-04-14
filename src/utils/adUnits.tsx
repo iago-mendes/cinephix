540f72cc096f961048662477ae955680784c8012
Iago <iagobrazmendes@gmail.com>
2021-04-14 11:07:19 -0300

    add scripts to ad units

--- src/utils/adUnits.tsx
+++ src/utils/adUnits.tsx
@@ -1,41 +1,68 @@
 export const LeaderboardAd: React.FC = () => (
-	<ins
-		className='adsbygoogle'
-		style=
-			{{
-				display: 'inline-block',
-				width: '728px',
-				height: '90px'
-			}}
-		data-ad-client='ca-pub-7920836956538831'
-		data-ad-slot='7349825410'
-	/>
+	<>
+		<script
+			async
+			src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js'
+		/>
+		<ins
+			className='adsbygoogle'
+			style=
+				{{
+					display: 'inline-block',
+					width: '728px',
+					height: '90px'
+				}}
+			data-ad-client='ca-pub-7920836956538831'
+			data-ad-slot='7349825410'
+		/>
+		<script>
+			(adsbygoogle = window.adsbygoogle || []).push({});
+		</script>
+	</>
 )
 
 export const MediumRectangleAd: React.FC = () => (
-	<ins
-		className='adsbygoogle'
-		style=
-			{{
-				display: 'inline-block',
-				width: '300px',
-				height: '250px'
-			}}
-		data-ad-client='ca-pub-7920836956538831'
-		data-ad-slot='7697309778'
-	/>
+	<>
+		<script
+			async
+			src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js'
+		/>
+		<ins
+			className='adsbygoogle'
+			style=
+				{{
+					display: 'inline-block',
+					width: '300px',
+					height: '250px'
+				}}
+			data-ad-client='ca-pub-7920836956538831'
+			data-ad-slot='7697309778'
+		/>
+		<script>
+			(adsbygoogle = window.adsbygoogle || []).push({});
+		</script>
+	</>
 )
 
 export const LargeMobileBannerAd: React.FC = () => (
-	<ins
-		className='adsbygoogle'
-		style=
-			{{
-				display: 'inline-block',
-				width: '320px',
-				height: '100px'
-			}}
-		data-ad-client='ca-pub-7920836956538831'
-		data-ad-slot='6001084721'
-	/>
+	<>
+		<script
+			async
+			src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js'
+		/>
+		<ins
+			className='adsbygoogle'
+			style=
+				{{
+					display: 'inline-block',
+					width: '320px',
+					height: '100px'
+				}}
+			data-ad-client='ca-pub-7920836956538831'
+			data-ad-slot='6001084721'
+		/>
+		<script>
+			(adsbygoogle = window.adsbygoogle || []).push({});
+		</script>
+	</>
 )
