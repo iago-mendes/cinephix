d454619b2f8eac651c7d0116789ee8360393230a
Iago <iagobrazmendes@gmail.com>
2021-07-02 21:54:02 -0300

    chore: remove next-auth

--- src/hooks/useUser.ts
+++ src/hooks/useUser.ts
@@ -1,9 +1,4 @@
-import {useSession} from 'next-auth/client'
 import {useEffect, useState} from 'react'
-import {
-	signIn as nextAuthSignIn,
-	signOut as nextAuthSignOut
-} from 'next-auth/client'
 
 interface User {
 	email: string
@@ -12,31 +7,12 @@ interface User {
 }
 
 function useUser() {
-	const [session, loading] = useSession()
+	const [loading, setLoading] = useState(true)
 	const [user, setUser] = useState<User | undefined>(undefined)
 
-	useEffect(() => {
-		if (!session) setUser(undefined)
-		else if (session.user.email) {
-			const tmpUser: User = {
-				email: session.user.email,
-				image: session.user.image || undefined,
-				name: session.user.name || undefined
-			}
+	function signIn() {}
 
-			setUser(tmpUser)
-		}
-	}, [session])
-
-	function signIn() {
-		const callbackUrl =
-			process.env.NEXT_PUBLIC_NEXTAUTH_URL + '/api/auth/callback/google'
-		nextAuthSignIn('google', {callbackUrl})
-	}
-
-	function signOut() {
-		nextAuthSignOut()
-	}
+	function signOut() {}
 
 	return {user, loading, signIn, signOut}
 }
