bf1b516614784e8f99e43f2e4f575fc4fd9542a9
Iago <iagobrazmendes@gmail.com>
2021-07-03 16:31:22 -0300

    chore: translate dates using date-fns

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,22 +1,26 @@
-export default function formatDate(unformatedDate: string | undefined) {
-	const months = [
-		'January',
-		'February',
-		'March',
-		'April',
-		'May',
-		'June',
-		'July',
-		'August',
-		'September',
-		'October',
-		'November',
-		'December'
-	]
+import {format, Locale} from 'date-fns'
+import {enUS, ptBR} from 'date-fns/locale'
+import {useRouter} from 'next/router'
 
+const locales = {
+	'en-US': enUS,
+	'pt-BR': ptBR
+}
+
+export function formatDate(unformatedDate: string | undefined) {
 	if (!unformatedDate || unformatedDate === '') return ''
 
-	const date = unformatedDate.split('-').map(s => Number(s))
-	const formatedDate = `${months[date[1] - 1]} ${date[2]}, ${date[0]}`
-	return formatedDate
+	const date = new Date(unformatedDate)
+
+	const {locale: routerLocale} = useRouter()
+	const locale: Locale = Object.keys(locales).includes(routerLocale)
+		? locales[routerLocale]
+		: enUS
+
+	const formatedDate = format(date, 'MMMM d, y', {locale})
+	const capitalizedDate =
+		formatedDate.charAt(0).toLocaleUpperCase() + formatedDate.slice(1)
+	return capitalizedDate
 }
+
+export default formatDate
