6b8d067351a87c157846838b2740c5643efa635e
Iago <iagobrazmendes@gmail.com>
2021-08-16 18:44:05 -0300

    fix: locale data

--- src/components/I18nHandler.tsx
+++ src/components/I18nHandler.tsx
@@ -3,13 +3,22 @@ import {i18n} from '@lingui/core'
 import {I18nProvider as LinguiProvider} from '@lingui/react'
 import {useRouter} from 'next/router'
 
+import {en, pt} from 'make-plural/plurals'
+
+i18n.loadLocaleData('en', {plurals: en})
+i18n.loadLocaleData('en-US', {plurals: en})
+i18n.loadLocaleData('pt', {plurals: pt})
+i18n.loadLocaleData('pt-BR', {plurals: pt})
+
 import {messages as enUSMessages} from '../locales/en-US/messages'
 import {messages as ptBRMessages} from '../locales/pt-BR/messages'
 
 i18n.load('en-US', enUSMessages)
+i18n.load('en', enUSMessages)
 i18n.load('pt-BR', ptBRMessages)
+i18n.load('pt', ptBRMessages)
 
-const validLocales = ['en-US', 'pt-BR']
+const validLocales = ['en-US', 'en', 'pt-BR', 'pt']
 
 type Props = {
 	children: ReactNode
